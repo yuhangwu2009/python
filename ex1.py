310ad0f5f8c5cf47e56e50ba6bb4981b9b9ed829
Yuhang Wu <yuhangwu2009@gmail.com>
2019-05-29 19:42:17 -0500

    exercise 1-2

---
+++ ex1.py
@@ -0,0 +1,13 @@
+print "Hello World!"
+print "Hello Again!"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print 'I d/ut know.'
+print 'I didn\\\\t know'
+print 'I did\know'
+print 'I did\iknow'
+print 'i did \oknow'
+print 'I did\y know'
