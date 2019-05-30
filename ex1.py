09de6244a20105a464b5458caf7533edf8e8c7ef
Yuhang Wu <yuhangwu2009@gmail.com>
2019-05-30 01:06:09 +0000

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,3 +1,5 @@
+# 5/29/19 8:00 by Yuhang aka me
+# Hi my future self!
 print "Hello World!"
 print "Hello Again!"
 print "I like typing this."
@@ -5,9 +7,3 @@ print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
-print 'I d/ut know.'
-print 'I didn\\\\t know'
-print 'I did\know'
-print 'I did\iknow'
-print 'i did \oknow'
-print 'I did\y know'
